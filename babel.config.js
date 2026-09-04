b9d899411b362fb86066f67260e7fc695b31ac7b
the-wilful-youth <thewilfulyouth@gmail.com>
2026-09-04 05:32:01 +0530

    chore: initialize project with Next.js, TypeScript, and testing setup

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+export default {
+  presets: [
+    ['next/babel'],
+    '@babel/preset-react',
+  ],
+};
---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Uncomment if using the App Router (default in Next 13+)
+  // experimental: { appDir: true },
+};
+export default nextConfig;
